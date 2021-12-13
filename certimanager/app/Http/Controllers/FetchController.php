1ca4ea4f894d2165281e4b7a51c9b1afafe54c59
Abdullah Aladham <abdullah.aladham@gmail.com>
2021-12-13 10:21:46 +0200

    db fetch module laravel

---
+++ certimanager/app/Http/Controllers/FetchController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\db_FetchModel;
+class FetchController extends Controller
+{
+    //
+
+  function  fetchexpired()
+  {
+      
+  }
+
+
+  function near_Expire_Certificates()
+  {
+    
+  }
+  public function getAllCertificates()
+  {
+
+  }
+}
--- certimanager/python/dbfetch.py
+++ certimanager/python/dbfetch.py
@@ -1,25 +1,28 @@
 #from bs4 import BeautifulSoup
 import json
+
 import requests
 import  mysql.connector
 #getting data through http get request
 req=requests.get('https://crt.sh/?q=islamicbank.ps&output=json')
-
-print(type(req))
+resp=req.json()
+#result=json.loads(req)
+#print(resp)
 #soup=BeautifulSoup(req,'html5lib')
 #datafound =soup.find('table',attrs={'tr'})
 #purgedata= datafound.find('td')
 #print(datafound)
-
-
+for i in req:
+    commonName=req['Common_name']
+    print(commonName)
 
 #establishing a connection to mysql
 conn= mysql.connector.connect(user="root",password="HelloWorld-1516!",host="localhost",database="cy_db")
 
 
 mydb=conn.cursor()
-#query="insert into certificates (Logged_At,Not_Before,Not_After,Common_Name,Matching_Identities,Issuer_Name"
-#mydb=conn.execute("")
+#query="INSERT INTO certificates (Issuer_Name,Common_Name,Logged_At,Not_Before,Not_After,Matching_Identities) Values()');"
+#coomand=mydb.execute(query)
 
 #for i in mydb:
  #   print(i)
